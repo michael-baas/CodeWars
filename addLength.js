6573a50ea954d1755c93dd0debc24214dd7d7808
Michael Baas <michaelbaas@me.com>
2022-05-07 11:14:28 -0400

    Add PREP

--- addLength.js
+++ addLength.js
@@ -2,10 +2,13 @@
 
 // addLength('apple ban') => ["apple 5", "ban 3"]
 // addLength('you will win') => ["you 3", "will 4", "win 3"]
-// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 //
 //Note: String will have at least one element; words will always be separated by a space.
 //
-// 
-//
-//
+// P: Parameters - strings that will have at least one element and
+// words are separated by space 
+// R: Returns - word with length added
+// E: Examples -  addLength('apple ban') => ["apple 5", "ban 3"]
+// P: Pseudocode - Loop through array, check each index's length then append that length it to the end of the index's string
+
+
