a73d15f0aac25321e485304acb5ebe45bba981cf
Michael Baas <michaelbaas@me.com>
2022-05-10 15:09:48 -0400

    Add PREP

--- solutons/binToDecimal.js
+++ solutons/binToDecimal.js
@@ -1,2 +1,8 @@
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 
+// P string of 1's and 0's to represent a binary number
+// R the decimal representation of the binary number.
+// E "1001001" -> 73
+// P Check each char of string to see if it's a 1 or a 0. If it's a 1
+// add 2^(index)
+
