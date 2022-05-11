705a11dee9fe0f2a2bf717b175e982708c4ca29c
Michael-Baas <michaelbaas@me.com>
2022-05-11 15:41:34 -0400

    Add solution

--- 8kyuSolutions/calculateBMI.js
+++ 8kyuSolutions/calculateBMI.js
@@ -12,4 +12,16 @@
 //E - ( 80, 1.8 ) -> "Normal"
 //P - Do BMI calculation then run if statements to figure out bmi
 //category
-
+function bmi(weight, height) {
+  let bmi = weight / (height**2)
+  if (bmi <= 18.5){
+    return "Underweight"
+  }else if (bmi <= 25.0){
+    return "Normal"
+  }else if (bmi <= 30){
+    return "Overweight"
+  }else{
+  return "Obese"  
+  }
+  return "";
+}
