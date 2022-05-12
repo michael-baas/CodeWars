b4bfad0ddfdc045ac063356d177e039362f5bb8b
Michael-Baas <michaelbaas@me.com>
2022-05-12 13:17:57 -0400

    Add PREP

--- 8kyuSolutions/reduceButGrow.js
+++ 8kyuSolutions/reduceButGrow.js
@@ -2,3 +2,9 @@
 //
 //[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
+// P - non-empty array of integers
+// R - the  result of multiplying each array item
+// E - [1,2,3,4a] => 1 * 2 * 3 * 4 = 24
+// P - use reduce method to multiply values
+
+
