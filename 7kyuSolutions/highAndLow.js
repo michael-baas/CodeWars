c602aa69e48d4aba5131a7fa17c46adebb2c040e
Michael-Baas <michaelbaas@me.com>
2022-05-13 11:39:10 -0400

    Add solution

--- 7kyuSolutions/highAndLow.js
+++ 7kyuSolutions/highAndLow.js
@@ -9,4 +9,14 @@
 // There will always be at least one number in the input string.
 // Output string must be two numbers separated by a single space, and highest number is first.
 
+// P - String of space separated numbers
+// R - the highest and lowest number in that string
+// E - see above
+// P - split string into array, spread array into Math.min()/ Math.max() 
 
+function highAndLow(numbers){
+  let ary = numbers.split(" ")
+  let min = Math.min(...ary)
+  let max - Math.max(...ary)
+  return `${max} ${min}`
+}
