1e0d3c869b8127558caa16637063185efb155d2c
Michael-Baas <michaelbaas@me.com>
2022-05-16 20:13:09 -0400

    Add solution

--- 6kyuSolutions/consecutiveStrings.js
+++ 6kyuSolutions/consecutiveStrings.js
@@ -30,3 +30,15 @@
 // Need to concatenate the array. Slice the array integer amount in each itteration 
 // Need to loop through the new array and figure out the longest string
 
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
+
