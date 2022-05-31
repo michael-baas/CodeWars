ab12ae133c2953173d197c99ce9326091ab1b2f4
Michael-Baas <michaelbaas@me.com>
2022-05-31 14:09:21 -0400

    Add solution

--- 8kyuSolutions/clock.js
+++ 8kyuSolutions/clock.js
@@ -13,3 +13,21 @@
 // 0 <= h <= 23
 // 0 <= m <= 59
 // 0 <= s <= 59
+
+// P - Hours, minutes and seconds 
+// R - Return time in milliseconds 
+// E - See above 
+// P - Convert all units of time measurement to milliseconds then add together
+
+function past(h, m, s) {
+    let hourMili, minuteMili, secMili, totalMili;
+    hourMili = h * 3.6e6;
+    minuteMili = m * 60000;
+    secMili = s * 1000;
+    totalMili = hourMili + minuteMili + secMili;
+    if (h < 0 || h > 23 || m < 0 || m > 59 || s < 0 || s > 59) {
+        return "Input outside of constraints";
+    } else {
+        return totalMili;
+    }
+}
