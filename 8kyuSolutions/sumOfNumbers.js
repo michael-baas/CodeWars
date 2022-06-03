02abb32d0777a6cd741f402fa12fd5124ca9fe69
Michael-Baas <michaelbaas@me.com>
2022-06-03 15:25:35 -0400

    Add solution

--- 8kyuSolutions/sumOfNumbers.js
+++ 8kyuSolutions/sumOfNumbers.js
@@ -15,3 +15,12 @@
 // E - See above
 // P - Find smallest then loop through and add together
 
+function getSum(a, b) {
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
