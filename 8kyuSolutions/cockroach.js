a288dd68a8c46d13e30965eeaf4a72428649fc42
Michael-Baas <michaelbaas@me.com>
2022-06-04 16:05:49 -0400

    Add solution

--- 8kyuSolutions/cockroach.js
+++ 8kyuSolutions/cockroach.js
@@ -4,3 +4,14 @@
 
 // 1.08 --> 30
 // Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+
+// P - number 
+// R - new speed in cm/s 
+// E - see above 
+// P need to multiply number by 27.7778 to return in cm/s
+
+function cockroachSpeed(s) {
+    //Good Luck!
+    return Math.floor(s * 27.7778);
+}
